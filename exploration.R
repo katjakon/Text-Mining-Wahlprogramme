b272eaa0e99d91389e2de859a9d22ce0cbf48186
a-klaus <76693508+a-klaus@users.noreply.github.com>
2021-03-28 18:37:12 +0200

    Update exploration.R

--- exploration.R
+++ exploration.R
@@ -517,3 +517,42 @@ gov_roots <- roots_for_group(sub_model_regierung, regierung_list, senti_dict)
 # optional: save to csv                                
 write.csv2(opp_roots, 'opp_roots_info.csv')
 write.csv2(gov_roots, 'gov_roots_info.csv')
+
+##################################################
+### extract sentiment based on value of column ###
+##################################################
+
+# function to collect sentiment for category
+# input: 
+# annotated_data - (subset of) corpus, annotated with udpipe
+# categoray
+# senti_data - a dataframe with two columns: term (filled with the terms...)
+# and value (their corresponding sentiment score
+collect_sentiment_for_cat <- function(annotated_data, category, senti_data) {
+  # filter data
+  filtered_data <- subset(annotated_data, upos == category)
+  filtered_data$senti_ws <- '-'
+  senti_df <- filtered_data[c(7,8)]
+  
+  senti_df$senti_ws <- '-'  # add sentiment column
+  for (w in unique(senti_df$lemma)) {
+    if (w %in% senti_data$term) {
+      if (length(senti_data[senti_data$term == w,]$value) < 2) {
+        senti_df[senti_df$lemma == w,]$senti_ws <- senti_data[senti_data$term == w,]$value
+      }
+    }
+  }
+  # count words
+  senti_df <- aggregate(cbind(senti_df[0],numdup=1), senti_df, length)
+  senti_df <- senti_df[order(senti_df$numdup, decreasing = TRUE),] %>%
+    filter(numdup > 1)
+  names(senti_df)[names(senti_df) == 'numdup'] <- 'freq'
+  return(senti_df)
+}
+                 
+opp_nouns <- collect_sentiment_for_cat(sub_model_opposition, 'NOUN', senti_dict)
+gov_nouns <- collect_sentiment_for_cat(sub_model_regierung, 'NOUN', senti_dict)
+
+head(opp_nouns)
+head(gov_nouns)           
+          
